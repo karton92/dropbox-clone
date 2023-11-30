99fe05a4a191f1ff4ba8ff32c84f1b255ea88d85
karton92 <michalaugustyn92@gmail.com>
2023-11-30 22:15:19 +0100

    Implementing Clerk authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <main className="">
+      <div>
+        <UserButton afterSignOutUrl="/" />
+      </div>
       <h1>Dropbox Clone</h1>
     </main>
   );
